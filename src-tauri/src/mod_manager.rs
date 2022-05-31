6e3c303d811e0c15679246dea19e32726957d468
MathiewMay <mathiewmaysenhoelder@hotmail.com>
2022-05-31 18:24:49 -0400

    finished ofs and some cleanup in mod_manager

--- src-tauri/src/mod_manager.rs
+++ src-tauri/src/mod_manager.rs
@@ -10,65 +10,64 @@ pub struct Game {
   path: String,
   work: String,
 }
-#[derive(Serialize, Deserialize, Clone)]
+
+#[derive(Serialize, Deserialize)]
 pub struct Mod {
   name: String,
   path: String,
 }
+
 #[tauri::command]
 pub(crate) fn deploy(mods: Vec<String>, game: String) {
   let mut mods_vec: Vec<Mod> = Vec::new();
-  let game_struct: Game = serde_json::from_str(game.as_str()).unwrap(); 
+  let game_struct: Game = serde_json::from_str(game.as_str()).unwrap();
   for string in mods {
     let json: Mod = serde_json::from_str(string.as_str()).unwrap();
     mods_vec.push(json);
   }
-  for mod_ in mods_vec.clone() {
-    println!("{}, {}", mod_.path, game_struct.name)
-  }
-  let ok = ofs::OFSLogic{ game: game_struct, mods: mods_vec };
-  ok.exec();
-  //let mod_u: Mod = serde_json::from_str(&mod_).unwrap();
+
+  let ofs = ofs::OFSLogic{ game: game_struct, mods: mods_vec };
+  ofs.exec();
 }
+
 #[tauri::command]
 pub(crate) fn scan_games() -> Vec<String> {
   let mut steam_games: Vec<String> = Vec::new();
   let home_dir = dirs::home_dir().unwrap();
   let steam_local_dir = home_dir.join(".local/share/Steam/steamapps/common/");
   let steam_flatpak_dir = home_dir.join(".var/app/com.valvesoftware.Steam/data/Steam/steamapps/common/");
-  let steam_dirs = [steam_local_dir, steam_flatpak_dir];
+  let mut steam_dirs: Vec<PathBuf> = vec!(steam_local_dir, steam_flatpak_dir);
+
+  let mnt_dir = PathBuf::new().join("/mnt");
+  for entry in mnt_dir.read_dir().unwrap() {
+    if let Ok (entry) = entry {
+      make_mnt_stage_directory(entry.path());
+      let mnt_steam_dir = entry.path().join("SteamLibrary/steamapps/common");
+      if mnt_steam_dir.exists() {
+        steam_dirs.push(mnt_steam_dir);
+      }
+    }
+  }
+
   for dir in steam_dirs {
     if dir.exists() {
-      let steam_directories = get_directories(dir);
+      let dir_string = dir.to_str().unwrap().to_string();
+      let steam_directories = get_directories(&dir);
       for game_entry in steam_directories {
         let game_entry_name = game_entry.file_name().unwrap().to_str().unwrap().to_string();
         let game_entry_path = game_entry.to_str().unwrap().to_string();
-        let work_path = home_dir.join(".config/tmm_stage/work/").join(game_entry_name.clone());
-        let game = Game {name: game_entry_name.clone(), path: game_entry_path, work: work_path.to_str().unwrap().to_string()};
+        let mut work_path = home_dir.join(".config/tmm_stage/work/");
+        if dir_string.starts_with("/mnt/") {
+          let work_prefix = dir_string.replace("SteamLibrary/steamapps/common", "");
+          work_path = PathBuf::new().join(work_prefix+".config/tmm_stage/work/");
+        }
+        make_work_directory(game_entry_name.clone(), work_path.clone());
+        let game = Game {name: game_entry_name.clone(), path: game_entry_path, work: work_path.join(game_entry_name).to_str().unwrap().to_string()};
         let json = serde_json::to_string(&game).unwrap();
         steam_games.push(json);
       }
     }
   }
-
-  let mnt_dir = PathBuf::new().join("/mnt");
-  for entry in mnt_dir.read_dir().unwrap() {
-    if let Ok (entry) = entry {
-      let mnt_steam_dir = entry.path().join("SteamLibrary/steamapps/common");
-      if mnt_steam_dir.exists() {
-        let mnt_directories = get_directories(mnt_steam_dir);
-        for game_entry in mnt_directories {
-          let game_entry_name = game_entry.file_name().unwrap().to_str().unwrap().to_string();
-          let game_entry_path = game_entry.to_str().unwrap().to_string();
-          let work_path = entry.path().join(".config/tmm_stage_work/").join(game_entry_name.clone());
-          let game = Game {name: game_entry_name.clone(), path: game_entry_path, work: work_path.to_str().unwrap().to_string()};
-          let json = serde_json::to_string(&game).unwrap();
-          steam_games.push(json);
-        }
-      }
-    }
-  }
-
   steam_games.into()
 }
 #[tauri::command]
@@ -99,7 +98,7 @@ pub(crate) fn make_game_stage_directory(game_name: String) {
 pub(crate) fn get_mods_name(game_name: String) -> Vec<String>{
   let join_path = ".config/tmm_stage/games/".to_owned()+game_name.as_str()+"/mods/";
   let game_dir = dirs::home_dir().unwrap().join(join_path);
-  let mods_directories = get_directories(game_dir);
+  let mods_directories = get_directories(&game_dir);
   let mut mods: Vec<String> = Vec::new();
   for path in mods_directories {
     let mod_entry_name = path.file_name().unwrap().to_str().unwrap().to_string();
@@ -110,7 +109,26 @@ pub(crate) fn get_mods_name(game_name: String) -> Vec<String>{
   }
   mods.into()
 }
-fn get_directories(path: PathBuf) -> Vec<PathBuf> {
+fn make_mnt_stage_directory(mnt: PathBuf) {
+  let config_dir = mnt.join(".config/");
+  let stage_dir = config_dir.join("tmm_stage/");
+  if !config_dir.exists() {
+    fs::create_dir(config_dir.clone()).unwrap();
+  }
+  if !stage_dir.exists() {
+    fs::create_dir(stage_dir).unwrap();
+  }
+}
+fn make_work_directory(game_name: String, prefix: PathBuf) {
+  if !prefix.exists() {
+    fs::create_dir(prefix.clone()).unwrap();
+  }
+  let work_dir = prefix.join(game_name);
+  if !work_dir.exists() {
+    fs::create_dir(work_dir).unwrap();
+  }
+}
+fn get_directories(path: &PathBuf) -> Vec<PathBuf> {
   let mut directories: Vec<PathBuf> = Vec::new();
   if path.exists() {
     for entry in path.read_dir().unwrap() {
--- src-tauri/src/mod_manager/ofs.rs
+++ src-tauri/src/mod_manager/ofs.rs
@@ -9,46 +9,43 @@ pub(crate) struct OFSLogic {
 
 impl OFSLogic {
   pub fn exec(&self) {
-    //before game here create the overlayfs
     let mut mod_paths: Vec<PathBuf> = Vec::new();
     let upper_path: PathBuf = PathBuf::new().join(&self.game.path);
-    let home_dir = dirs::home_dir().unwrap();
-    let mount_path: PathBuf = home_dir.clone().join(".config/tmm_stage/mount/").join(&self.game.name);
-    let tmp_path: PathBuf = home_dir.clone().join(&self.game.work);
-    for mod_ in &self.mods {
-      mod_paths.push(PathBuf::new().join(&mod_.path));
+    let work_path: PathBuf = dirs::home_dir().unwrap().join(&self.game.work);
+    
+    for elem in &self.mods {
+      mod_paths.push(PathBuf::new().join(&elem.path));
     }
-    //make sure to create the directories before running init overlay fs
-    init_overlay_fs(mod_paths, upper_path, mount_path, tmp_path);
-    //run game here
+
+    init_overlay_fs(mod_paths, &upper_path, &upper_path, &work_path);
   }
 }
 
-fn init_overlay_fs(lower: Vec<PathBuf>, upper: PathBuf, mount: PathBuf, workdir: PathBuf) {
-  println!("Started a overlay fs init");
-  let mut mod_dirs: String = String::from("lowerdir=");
-  let game_dir: String = String::from("upperdir=").to_owned()+upper.to_str().unwrap();
-  let tmp_dir: String = String::from("workdir=").to_owned()+workdir.to_str().unwrap();
+fn init_overlay_fs(lower: Vec<PathBuf>, upper: &PathBuf, mount: &PathBuf, workdir: &PathBuf) {
+  let mut lower_arg: String = String::from("lowerdir=");
+  let upper_arg: String = String::from("upperdir=").to_owned()+upper.to_str().unwrap();
+  let work_arg: String = String::from("workdir=").to_owned()+workdir.to_str().unwrap();
+
   let mut index = 0;
   for path in &lower {
-    mod_dirs.push_str(path.to_str().unwrap());
+    lower_arg.push_str(path.to_str().unwrap());
     index = index + 1;
     if index < lower.len() {
-      mod_dirs.push(':');
+      lower_arg.push(':');
     }
   }
-  println!("mount -t overlay overlay -o {} -o {} -o {} {}", mod_dirs, game_dir, tmp_dir, mount.to_str().unwrap());
+  
   Command::new("pkexec")
     .arg("mount")
     .arg("-t")
     .arg("overlay")
     .arg("overlay")
     .arg("-o")
-    .arg(&mod_dirs)
+    .arg(&lower_arg)
     .arg("-o")
-    .arg(game_dir)
+    .arg(upper_arg)
     .arg("-o")
-    .arg(tmp_dir)
+    .arg(work_arg)
     .arg(mount.to_str().unwrap())
     .spawn()
     .unwrap();
